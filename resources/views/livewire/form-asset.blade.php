64f1021b20b5f4a5f5e6791db627d5b70b3034c7
masif088 <mokhamadasif@gmail.com>
2021-12-05 20:50:12 +0700

    update asset and finance

--- resources/views/livewire/form-asset.blade.php
+++ resources/views/livewire/form-asset.blade.php
@@ -4,14 +4,15 @@
             <x-select :options="$optionProductType" :selected="$data['product_type_id']" title="Usaha"
                       model="data.product_type_id"/>
         @endif
-        <x-select :options="$optionState" :selected="$data['asset_state_id']" title="Kondisi"
-                  model="data.asset_state_id"/>
-        <x-select :options="$optionCode" :selected="$data['asset_code_id']" title="Kode" model="data.asset_code_id"/>
-        <x-select :options="$optionOwnership" :selected="$data['asset_ownership_id']" title="Kode"
+        <x-select :options="$optionCode" :selected="$data['asset_code_id']" title="Jenis Asset"
+                  model="data.asset_code_id"/>
+        <x-select :options="$optionOwnership" :selected="$data['asset_ownership_id']" title="Kepemilikan"
                   model="data.asset_ownership_id"/>
         <x-input type="text" model="data.title" title="Nama asset"/>
         <x-input type="number" model="data.amount" title="Jumlah asset"/>
         <x-input type="number" model="data.nominal" title="Nominal"/>
+        <x-select :options="$optionState" :selected="$data['asset_state_id']" title="Kondisi"
+                  model="data.asset_state_id"/>
         <div class="form-group col-span-6 sm:col-span-5"></div>
         <button type="submit" id="submit" class="btn btn-primary">Submit</button>
     </form>
