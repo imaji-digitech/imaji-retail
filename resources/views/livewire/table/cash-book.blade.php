718eaa37de48a3b7c2a6a351ade09d691b52acfb
masif088 <mokhamadasif@gmail.com>
2021-12-11 10:46:11 +0700

    update so many :)

--- resources/views/livewire/table/cash-book.blade.php
+++ resources/views/livewire/table/cash-book.blade.php
@@ -2,46 +2,53 @@
     <x-data-table :data="$data" :model="$cashBooks">
         <x-slot name="head">
             <tr>
-                <th style="width: 200px !important;">
+                <th style="padding:0;margin:0;width: 140px !important;">
                     Tanggal
                 </th>
-                <th style="width: 120px !important;">
+                <th style="padding:0;margin:0;width: 60px !important;">
                     Kode
                 </th>
-                <th style="width: 170px !important;">
+                <th style="padding:0;margin:0;">
                     Keterangan
                 </th>
-                <th style="width: 170px !important;">
+                <th style="padding:0;margin:0;width: 100px !important;">
                     Masuk
                 </th>
-                <th style="width: 170px !important;">
+                <th style="padding:0;margin:0;width: 100px !important;">
                     Keluar
                 </th>
-                <th style="width: 170px !important;">
+                <th style="padding:0;margin:0;width: 100px !important;">
                     Saldo
                 </th>
-                <th style="width: 100px">
+                <th style="padding:10px;margin:0;width: 70px">
                     Action
                 </th>
             </tr>
         </x-slot>
         <x-slot name="body">
             @php
-                $c=App\Models\CashNote::whereProductTypeId($dataId)->orderByDesc('id')->first();
+                $c=App\Models\CashNote::whereProductTypeId($dataId)->orderByDesc('id')->first()
             @endphp
             @foreach ($cashBooks as $cashBook)
                 <tr x-data="window.__controller.dataTableController({{ $cashBook->id }})">
-                    <td style="height: 10px !important;">{{ $cashBook->created_at->format('H:m d/M/Y') }}</td>
-                    <td style="height: 10px !important;">{{ str_pad($cashBook->code_cash_book_id, 3, '0', STR_PAD_LEFT) }}</td>
-                    <td style="height: 10px !important;">{{ $cashBook->note }}</td>
-                    <td style="height: 10px !important;">Rp {{ number_format($cashBook->income,0,",",".") }}</td>
-                    <td style="height: 10px !important;">Rp {{ number_format($cashBook->outcome,0,",",".") }}</td>
-                    <td style="height: 10px !important;">Rp {{ number_format($c->balance=$c->balance+$cashBook->income-$cashBook->outcome,0,",",".") }}</td>
-                    <td style="height: 10px !important;">
+                    <td style="padding:0;margin:0;height: 10px !important;">
+                        {{ $cashBook->created_at->format('H:m d/M/Y') }}</td>
+                    <td style="padding:0;margin:0;text-align:center;height: 10px !important;">
+                        {{ str_pad($cashBook->code_cash_book_id, 3, '0', STR_PAD_LEFT) }}</td>
+                    <td style="padding:0;margin:0;height: 10px !important;">
+                        {{ $cashBook->note }}</td>
+                    <td style="padding:0;margin:0;height: 10px !important;">
+                        Rp {{ number_format($cashBook->income,0,",",".") }}</td>
+                    <td style="padding:0;margin:0;height: 10px !important;">
+                        Rp {{ number_format($cashBook->outcome,0,",",".") }}</td>
+                    <td style="padding:0;margin:0;height: 10px !important;">
+                        Rp {{ number_format($c->balance=$c->balance+$cashBook->income-$cashBook->outcome,0,",",".") }}</td>
+                    <td style="padding:10px;margin:0;height: 10px !important;">
                         @if($cashBook->code_cash_book_id!=1 and $cashBook->code_cash_book_id!=999)
                             <a role="button" x-on:click.prevent="deleteItem" href="#" class="mr-3">
                                 <i class="fa fa-16px fa-trash text-red-500"></i></a>
-                            <a role="button" href="{{ route('admin.cash-book.edit',[$dataId,$cashBook->id]) }}" class="mr-3">
+                            <a role="button" href="{{ route('admin.cash-book.edit',[$dataId,$cashBook->id]) }}"
+                               class="mr-3">
                                 <i class="fa fa-16px fa-pencil text-blue-500"></i>
                             </a>
                         @endif
--- resources/views/livewire/table/product.blade.php
+++ resources/views/livewire/table/product.blade.php
@@ -3,56 +3,57 @@
     <x-data-table :data="$data" :model="$products">
         <x-slot name="head">
             <tr>
-                <th style="width: 20px">#</th>
-                <th style="width: 300px !important;">
+                <th style="padding:5px;margin:5px; width: 20px">#</th>
+                <th style="padding:0;margin:0; !important;">
                     <a wire:click.prevent="sortBy('title')" role="button" href="#">
-                        Nama Produk @include('components.sort-icon', ['field' => 'title'])
+                        Nama  @include('components.sort-icon', ['field' => 'title'])
                     </a>
                 </th>
-                <th style="width: 170px !important;">
-                    <a wire:click.prevent="sortBy('product_type_id')" role="button" href="#">
-                        UMKM
-                        @include('components.sort-icon', ['field' => 'product_type_id'])
-                    </a>
-                </th>
-                <th style="width: 170px !important;">
+                <th style="padding:0;margin:0;width: 100px!important;">
                     <a wire:click.prevent="sortBy('status_id')" role="button" href="#">
-                        Kode Produk @include('components.sort-icon', ['field' => 'code'])
+                        SKU
+                        @include('components.sort-icon', ['field' => 'code'])
                     </a>
                 </th>
-                <th style="width: 170px !important;">
+                <th style="padding:0;margin:0;width: 100px !important;">
                     <a wire:click.prevent="sortBy('price')" role="button" href="#">
-                        Harga Produk
+                        Harga
                         @include('components.sort-icon', ['field' => 'price'])
                     </a>
                 </th>
-                <th style="width: 170px !important;">
+                <th style="padding:0;margin:0;width: 100px !important;">
                     <a wire:click.prevent="sortBy('hpp')" role="button" href="#">
-                        HPP Produk
+                        HPP
                         @include('components.sort-icon', ['field' => 'hpp'])
                     </a>
                 </th>
-                <th style="width: 170px !important;">
+                <th style="padding:0;margin:0;width: 100px !important;">
                     <a wire:click.prevent="sortBy('hpp')" role="button" href="#">
-                        Profit Produk
+                        Profit
                         @include('components.sort-icon', ['field' => 'hpp'])
                     </a>
                 </th>
-                <th style="width: 170px !important;">
+                <th style="padding:0;margin:0;width: 50px !important;">
                     <a wire:click.prevent="sortBy('stock')" role="button" href="#">
-                        Stock Produk
+                        <i class="fa fa-16px fa-files-o text-blue-500"></i>
                         @include('components.sort-icon', ['field' => 'stock'])
                     </a>
                 </th>
-                <th style="width: 230px !important;">
-                    <a wire:click.prevent="sortBy('updated_at')" role="button" href="#">
-                        Terakhir diperbarui
-                        @include('components.sort-icon', ['field' => 'updated_at'])
-                    </a>
+                <th style="padding:0;margin:0;width: 50px !important;">
+                    <i class="fa fa-16px fa-shopping-cart text-blue-500"></i>
                 </th>
+                <th style="padding:0;margin:0; width: 50px !important;">
+                    <i class="fa fa-16px fa-shopping-cart text-blue-500"></i><i class="fa fa-16px fa-calendar text-blue-500"></i>
+                </th>
+                {{--                <th style="padding:0;margin:0;width: 230px !important;">--}}
+                {{--                    <a wire:click.prevent="sortBy('updated_at')" role="button" href="#">--}}
+                {{--                        Terakhir diperbarui--}}
+                {{--                        @include('components.sort-icon', ['field' => 'updated_at'])--}}
+                {{--                    </a>--}}
+                {{--                </th>--}}
 
-                <th style="width: 400px">
-                    Action
+                <th style="padding:0;margin:0;width: 150px">
+                    Aksi
                 </th>
             </tr>
         </x-slot>
@@ -60,40 +61,63 @@
         <x-slot name="body">
             <form action="{{route('admin.product.graph')}}" method="post">
                 @csrf
-
                 @foreach ($products as $product)
-                    <tr x-data="window.__controller.dataTableController({{ $product->id }})">
-                        <td style="height: 10px !important;">
+                    <tr x-data="window.__controller.dataTableController({{ $product->id }})" style="">
+                        <td style="padding:0;margin:0;height: 10px !important;">
                             <input type="checkbox" value="{{$product->id}}" name="productId[]">
                         </td>
-                        <td style="height: 10px !important;">{{ $product->title }}</td>
-                        <td style="height: 10px !important;">{{ $product->productType->title }}</td>
-                        <td style="height: 10px !important;">{{ $product->code }}</td>
-                        <td style="height: 10px !important;">Rp {{ number_format($product->price,0,",",".") }}</td>
-                        <td style="height: 10px !important;">Rp {{ number_format($product->hpp,0,",",".") }}</td>
-                        <td style="height: 10px !important;">Rp {{ number_format($product->price-$product->hpp,0,",",".") }}</td>
-                        <td style="height: 10px !important;">{{ $product->stock }}</td>
-                        <td style="height: 10px !important;">{{ $product->updated_at?$product->updated_at->format('d M Y'):'' }}</td>
-                        <td style="height: 10px !important;" class="whitespace-no-wrap row-action--icon">
-                            <a role="button" href="{{ route('admin.product.manufacture',[$product->product_type_id,$product->id]) }}" class="mr-3">
+                        <td style="padding:0;margin:0;height: 10px !important;">{{ $product->title }}</td>
+                        <td style="padding:0;margin:0;height: 10px !important;">{{ $product->code }}</td>
+                        <td style="padding:0;margin:0;height: 10px !important;">Rp {{ number_format($product->price,0,",",".") }}</td>
+                        <td style="padding:0;margin:0;height: 10px !important;">Rp {{ number_format($product->hpp,0,",",".") }}</td>
+                        <td style="padding:0;margin:0;height: 10px !important;">
+                            Rp {{ number_format($product->price-$product->hpp,0,",",".") }}</td>
+                        <td style="padding:0;margin:0;height: 10px !important;">{{ $product->stock }}</td>
+                        @php($tp=0)
+                        @foreach($product->transactionDetails as $td)
+                            @if($td->created_at->month== \Carbon\Carbon::now()->month)
+                            @php($tp+=$td->quantity)
+                            @endif
+                        @endforeach
+                        <td style="padding:0;margin:0;height: 10px !important;">{{ $product->transactionDetails->sum('quantity') }}</td>
+
+                        <td style="padding:0;margin:0;height: 10px !important;">{{ $tp }}</td>
+                        {{--                        <td style="padding:0;margin:0;height: 10px !important;">{{ $product->updated_at?$product->updated_at->format('d M Y'):'' }}</td>--}}
+                        <td style="padding:0;margin:0;height: 10px !important;" class="whitespace-no-wrap row-action--icon">
+                            <a role="button"
+                               href="{{ route('admin.product.manufacture',[$product->product_type_id,$product->id]) }}"
+                               class="mr-3">
                                 <i class="fa fa-16px fa-money text-blue-500"></i>
                             </a>
-                            <a role="button" href="{{ route('admin.product.edit',[$product->product_type_id,$product->id]) }}" class="mr-3">
+                            <a role="button"
+                               href="{{ route('admin.product.edit',[$product->product_type_id,$product->id]) }}"
+                               class="mr-3">
                                 <i class="fa fa-16px fa-pen text-blue-500"></i>
                             </a>
-                            <a role="button" href="{{ route('admin.product.show',[$product->product_type_id,$product->id]) }}" class="mr-3"
+
+                            <a role="button"
+                               href="{{ route('admin.product.show',[$product->product_type_id,$product->id]) }}"
+                               class="mr-3"
                                target="_blank">
                                 <i class="fa fa-16px fa-bar-chart text-blue-500"></i>
                             </a>
-                            <a role="button" href="{{ route('admin.product.export',[$product->product_type_id,$product->id]) }}" class="mr-3"
+                            <br>
+                            <a role="button"
+                               href="{{ route('admin.product.export',[$product->product_type_id,$product->id]) }}"
+                               class="mr-3"
                                target="_blank">
                                 <i class="fa fa-16px fa-download text-danger">PDF</i>
                             </a>
-                            <a role="button" href="{{ route('admin.product.stock',[$product->product_type_id,$product->id]) }}" class="mr-3"
+                            <a role="button"
+                               href="{{ route('admin.product.stock',[$product->product_type_id,$product->id]) }}"
+                               class="mr-3"
                                target="_blank">
                                 <i class="fa fa-16px fa-plus text-blue-500">STOCK</i>
                             </a>
-                            <a role="button" href="{{ route('admin.product.history',[$product->product_type_id,$product->id]) }}" class="mr-3"
+                            <br>
+                            <a role="button"
+                               href="{{ route('admin.product.history',[$product->product_type_id,$product->id]) }}"
+                               class="mr-3"
                                target="_blank">
                                 <i class="fa fa-16px fa-copy text-blue-500">History</i>
                             </a>
