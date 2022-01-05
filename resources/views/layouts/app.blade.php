f120ea4250533fc50839aa19cb15f8d238a92091
masif088 <mokhamadasif@gmail.com>
2022-01-05 14:27:49 +0700

    font awesome issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -25,8 +25,9 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <link rel="stylesheet" href="{{asset('vendor/bootstrap-timepicker/css/bootstrap-timepicker.min.css')}}">
 
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.0/css/v4-shims.css" media="all">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.0/css/v4-font-face.min.css" media="all">
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+{{--        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.0/css/v4-shims.css" media="all">--}}
+{{--        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.0/css/v4-font-face.min.css" media="all">--}}
 {{--        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.10.0/css/min.css" media="all">--}}
 {{--        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/duotone.css" integrity="sha384-R3QzTxyukP03CMqKFe0ssp5wUvBPEyy9ZspCB+Y01fEjhMwcXixTyeot+S40+AjZ" crossorigin="anonymous"/>--}}
 {{--        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/fontawesome.css" integrity="sha384-eHoocPgXsiuZh+Yy6+7DsKAerLXyJmu2Hadh4QYyt+8v86geixVYwFqUvMU8X90l" crossorigin="anonymous"/>--}}
