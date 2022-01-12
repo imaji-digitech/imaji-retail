5b5382d69925e211a9527c3c6c19324c2a1b961e
masif088 <mokhamadasif@gmail.com>
2022-01-12 09:11:45 +0700

    font awesome issue

--- resources/views/pages/cash-note/show.blade.php
+++ resources/views/pages/cash-note/show.blade.php
@@ -4,8 +4,7 @@
 
         <div class="section-header-breadcrumb">
             <div class="breadcrumb-item active"><a href="{{ route('admin.dashboard') }}">{{__('dashboard')}}</a></div>
-            <div class="breadcrumb-item"><a href="{{ route('admin.product.index') }}">{{__('Produk')}}</a></div>
-            <div class="breadcrumb-item"><a href="#">{{__('Detail produk')}}</a></div>
+            <div class="breadcrumb-item"><a href="#">{{__('Detail Laporan')}}</a></div>
         </div>
     </x-slot>
 
